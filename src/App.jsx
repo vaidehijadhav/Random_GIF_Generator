fc93640a1577e6c6b26a8a9927927a35ccf158c7
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-04-09 22:49:21 +0530

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Tag from "./components/Tag";
 
 export default function App() {
   return (
-    <div className="w-full h-screen flex flex-col background relative">
-      <h1 className="absolute bg-white rounded-sm">Random GIF</h1>
-      <div className="flex flex-col">
+    <div className="w-full h-screen flex flex-col background relative overflow-hidden items-center">
+      <h1 className="bg-white rounded-lg w-11/12 text-center mt-[40px] font-bold text-4xl px-10 py-3">
+        Random GIF
+      </h1>
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
         <Random/>
         <Tag/>
       </div>
