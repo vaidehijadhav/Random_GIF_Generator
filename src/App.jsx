ecec2c71dab2d806abfa05a7d84a565f068836ec
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-04-09 19:33:51 +0530

    create stucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import Random from "./components/Random";
+import Tag from "./components/Tag";
+
 export default function App() {
-  return <div>Random GIF</div>;
+  return (
+    <div className="w-full h-screen flex flex-col background relative">
+      <h1 className="absolute bg-white rounded-sm">Random GIF</h1>
+      <div className="flex flex-col">
+        <Random/>
+        <Tag/>
+      </div>
+    </div>
+  );
 }
